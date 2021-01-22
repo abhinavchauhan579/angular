9e84a83a497cd8af7c3a4bb05e0eaf0928d8b21e
Abhinav Chauhan <42146237+abhinavchauhan579@users.noreply.github.com>
2021-01-22 15:34:28 +0530

    Added more content in form essentials

--- src/app/form-essentials/form-essentials.component.ts
+++ src/app/form-essentials/form-essentials.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
   selector: 'app-form-essentials',
@@ -15,19 +16,37 @@ export class FormEssentialsComponent implements OnInit {
   public checkedState = false;
   public indeterminateState = false;
   public disabledState = false;
+  public languageList: any;
+  public dropList: any;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
 
     this.controlsForm = this.fb.group({
+
       checkedCheckbox: new FormControl(false),
       indeterminateCheckbox: new FormControl(false),
       disabledCheckbox: new FormControl(false),
       resultCheckbox: new FormControl(false),
+
       startDate: new FormControl(null, Validators.required),
-      endDate: new FormControl(null, Validators.required)
+      endDate: new FormControl(null, Validators.required),
+
+      dropdown: new FormControl(null, Validators.required)
     });
+
+    this.languageList = [
+      {name: 'Python'},
+      {name: 'C++'},
+      {name: 'Java'},
+    ];
+
+    this.dropList = [
+      {id: '1', name: 'English'},
+      {id: '2', name: 'Hindi'},
+      {id: '3', name: 'French'},
+    ];
 
     this.controlsForm.get('checkedCheckbox').valueChanges.subscribe((value: any) => {
       if(value === true) {
@@ -68,6 +87,27 @@ export class FormEssentialsComponent implements OnInit {
       }
     });
 
+  }
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our language
+    if ((value || '').trim()) {
+      this.languageList.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(language: any): void {
+    const index = this.languageList.indexOf(language);
+    if (index >= 0) {
+      this.languageList.splice(index, 1);
+    }
   }
 
 }
