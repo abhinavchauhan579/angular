39300f5069791bd7095a71fbf7f4c82b051891b1
Abhinav Chauhan <42146237+abhinavchauhan579@users.noreply.github.com>
2021-02-26 12:34:18 +0530

    Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AlertsComponent } from './alerts/alerts.component';
 import { FormEssentialsComponent } from './form-essentials/form-essentials.component';
+import { HomeComponent } from './home/home.component';
 import { TablesComponent } from './tables/tables.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo:'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home'}
+  },
   {
     path: 'forms',
     component: FormEssentialsComponent,
